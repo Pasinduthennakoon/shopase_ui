cb4a1b4a8a979a943de6b31bfd83c47bb1824d23
pasindu P thennakoon <pasinduthennakoon512@gmail.com>
2025-06-20 23:55:40 +0530

    configurations

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,12 +1,14 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
+import type { Config } from 'tailwindcss'
+
+export default {
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
     "./public/index.html",
   ],
+
   theme: {
     extend: {},
   },
   plugins: [],
-}
+} satisfies Config
 
