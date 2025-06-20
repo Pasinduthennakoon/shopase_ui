f06dcef819862d72f0c3fc162fc105e0fceda3bc
pasindu P thennakoon <pasinduthennakoon512@gmail.com>
2025-06-21 01:08:23 +0530

    update

---
+++ src/Shop.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Navigation from './components/Navigations/Navigation'
+import './Shop.css'
+
+export const Shop = () => {
+  return (
+    <>
+      <Navigation />
+    </>
+  )
+}
+
+export default Shop;
+
