6964f4858c824ad61e529ca368fa1cc7270131f4
pasindu P thennakoon <pasinduthennakoon512@gmail.com>
2025-06-21 01:34:22 +0530

    create herosection

--- src/Shop.jsx
+++ src/Shop.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import Navigation from './components/Navigations/Navigation'
 import './Shop.css'
+import HeroSection from './components/HeroSection/HeroSection'
 
 export const Shop = () => {
   return (
     <>
       <Navigation />
+      <HeroSection />
     </>
   )
 }
