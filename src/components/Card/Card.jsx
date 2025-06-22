428e8884e19d5f289cdb90b9f682d59b44d84527
pasindu P thennakoon <pasinduthennakoon512@gmail.com>
2025-06-23 01:48:03 +0530

    create categories and content.json

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,10 +1,16 @@
 import React from 'react'
 
-const Card = ({imagePath, title}) => {
+const Card = ({imagePath, title, description, actionArrow}) => {
   return (
     <div className='flex flex-col p-8'>
         <img className='max-h-[260px] max-w-[240px] bg-cover bg-center border rounded hover:scale-105 cursor-pointer' src={imagePath} alt="Jeans" />
-        <p className='text-[16px] p-[5px]'>{title}</p>
+        <div className='flex justify-between'>
+            <div className='flex flex-col p-2'>
+                <p className='text-[16px]'>{title}</p>
+                {description && <p className='text-[14px]'>{description}</p>}
+            </div>
+            {actionArrow && <div></div>}
+        </div>
     </div>
   )
 }
