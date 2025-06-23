303dc8a2a1cbc16a291b3f5ced3470962165818f
pasindu P thennakoon <pasinduthennakoon512@gmail.com>
2025-06-23 23:05:52 +0530

    design footer

--- src/Shop.jsx
+++ src/Shop.jsx
@@ -5,6 +5,7 @@ import HeroSection from './components/HeroSection/HeroSection'
 import NewArrivals from './components/Section/NewArrivals'
 import Category from './components/Categories/Category'
 import content from './data/content.json'
+import Footer from './components/Footer/Footer'
 
 export const Shop = () => {
   return (
@@ -13,7 +14,7 @@ export const Shop = () => {
       <HeroSection />
       <NewArrivals />
       {content?.categories && content?.categories?.map((item, index) => <Category key={item?.title+index} {...item}/>)}
-
+      <Footer content={content?.footer}/>
     </>
   )
 }
--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,8 +1,27 @@
 import React from 'react'
+import FbIcon from '../common/FbIcon'
+import InstaIcon from '../common/InstaIcon'
 
-const Footer = () => {
+const Footer = ({content}) => {
   return (
-    <div>Footer</div>
+    <div className='bg-black text-white'>
+        <div className='flex p-8 justify-around'>
+            {content?.items && content?.items?.map((item,index) =>{
+                return(
+                    <div className='flex flex-col'>
+                        <p className='text-[16px] pb-[10px]'>{item?.title}</p>
+                        {item?.list && item?.list?.map((listitem,index) =><a className='flex flex-col text-[12px] py-2' href={listitem?.path}>{listitem?.label}</a>)}
+                        {item?.description && <p>{item?.description}</p>}
+                    </div>
+                )
+            })}
+        </div>
+        <div className='flex gap-2 items-center justify-center py-4'>
+            <a href='/fb'><FbIcon /> </a>
+            <a href='/insta'><InstaIcon /> </a>
+        </div>
+        <p className='text-sm text-white text-center content-center'>{content?.copyright}</p>
+    </div>
   )
 }
 
