23db1f2e2ca2fd13860ba80d48f0d9993144f596
pasindu P thennakoon <pasinduthennakoon512@gmail.com>
2025-06-23 19:29:22 +0530

    update card.jsx

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,13 +1,13 @@
 import React from 'react'
 
-const Card = ({imagePath, title, description, actionArrow}) => {
+const Card = ({imagePath, title, description, actionArrow, height, width}) => {
   return (
     <div className='flex flex-col p-8'>
-        <img className='max-h-[260px] max-w-[240px] bg-cover bg-center border rounded hover:scale-105 cursor-pointer' src={imagePath} alt="Jeans" />
+        <img className={`h-[${height? height:'220px'}] max-h-[${height? height:'260px'}] w-[${width? width:'200px'}] max-w-[${width? width:'220px'}] bg-cover bg-center border rounded hover:scale-105 cursor-pointer`} src={imagePath} alt="Jeans" />
         <div className='flex justify-between'>
             <div className='flex flex-col p-2'>
-                <p className='text-[16px]'>{title}</p>
-                {description && <p className='text-[14px]'>{description}</p>}
+                <p className='text-[16px] p-1'>{title}</p>
+                {description && <p className='text-[12px] p-1 text-gray-600'>{description}</p>}
             </div>
             {actionArrow && <div></div>}
         </div>
