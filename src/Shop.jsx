b4b3c8969ee78d8e4aa6848188448fa54d3f1dac
pasindu P thennakoon <pasinduthennakoon512@gmail.com>
2025-06-23 19:30:15 +0530

    update category

--- src/Shop.jsx
+++ src/Shop.jsx
@@ -3,6 +3,8 @@ import Navigation from './components/Navigations/Navigation'
 import './Shop.css'
 import HeroSection from './components/HeroSection/HeroSection'
 import NewArrivals from './components/Section/NewArrivals'
+import Category from './components/Categories/Category'
+import content from './data/content.json'
 
 export const Shop = () => {
   return (
@@ -10,6 +12,8 @@ export const Shop = () => {
       <Navigation />
       <HeroSection />
       <NewArrivals />
+      {content?.categories && content?.categories?.map((item, index) => <Category key={item?.title+index} {...item}/>)}
+
     </>
   )
 }
--- src/components/Categories/Category.jsx
+++ src/components/Categories/Category.jsx
@@ -1,18 +1,19 @@
 import React from 'react'
 import SeactionHeading from '../Section/SectionHeading/SeactionHeading'
+import Card from '../Card/Card'
 
-const Category = ({title,data}) => {
+const Category = ({ title, data }) => {
   return (
     <>
-        <SeactionHeading title={'title'}/>
-        <div className='flex items-center px-8 flex-wrap'>
-    {data && data?.map((item,index)=>{
-        return (
+      <SeactionHeading title={title} />
+      <div className='flex items-center px-8 flex-wrap'>
+        {data && data?.map((item, index) => {
+          return (
             <Card key={index} title={item?.title} description={item?.description} imagePath={item?.image}
-             actionArrow={true} height={'240px'} width={'200px'}/>
-        )
-    })}
-    </div>
+              actionArrow={true} height={'280px'} width={'200px'} />
+          )
+        })}
+      </div>
     </>
   )
 }
