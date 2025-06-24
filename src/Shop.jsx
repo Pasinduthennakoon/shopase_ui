cf444b66a45612a7cc13732eb8230703e287dac6
pasindu P thennakoon <pasinduthennakoon512@gmail.com>
2025-06-25 02:33:43 +0530

    change content.json stucture and load it's component to webapplication

--- src/Shop.jsx
+++ src/Shop.jsx
@@ -5,18 +5,18 @@ import NewArrivals from './components/Section/NewArrivals'
 import Category from './components/Categories/Category'
 import content from './data/content.json'
 import Footer from './components/Footer/Footer'
-import Navigation from './components/Navigations/Navigation'
 
 const Shop = () => {
   return (
     <>
       <HeroSection />
       <NewArrivals />
-      {content?.categories && content?.categories?.map((item, index) => <Category key={item?.title+index} {...item}/>)}
+      {content?.pages?.shop?.sections && content?.pages?.shop?.sections?.map((item, index) => <Category key={`${item.title}-${index}`} {...item} />)}
       <Footer content={content?.footer}/>
     </>
   )
 }
 
 export default Shop;
+
 
