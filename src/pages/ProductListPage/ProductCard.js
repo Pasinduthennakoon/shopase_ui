0e5194182539d31feed797a239b308d75ae2d66e
pasindu P thennakoon <pasinduthennakoon512@gmail.com>
2025-06-27 22:44:44 +0530

    update content.js

---
+++ src/pages/ProductListPage/ProductCard.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import SvgFavourite from '../../components/common/SvgFavourite'
+import { Link } from 'react-router-dom'
+
+const ProductCard = ({ id, title, description, price, discoutnt, rating, brand, thumbnail, slug }) => {
+    return (
+        <div className='flex flex-col hover:scale-105 relative'>
+            <Link to={`/product/${id}`}>
+            <img className={'h-[320px] w-[280px] bg-cover bg-center border rounded cursor-pointer object-cover block'} src={thumbnail} alt="Jeans" />
+            </Link>
+
+            <div className='flex justify-between items-center'>
+                <div className='flex flex-col p-2'>
+                    <p className='text-[16px] p-1'>{title}</p>
+                    {description && <p className='text-[12px] p-1 text-gray-600'>{brand}</p>}
+                </div>
+                <div>
+                    <p>${price}</p>
+                </div>
+            </div>
+            <button className='absolute top-0 right-0 pt-2 pr-4 cursor-pointer'><SvgFavourite /></button>
+        </div>
+    )
+}
+
+export default ProductCard
