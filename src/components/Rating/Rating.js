3badb918d33ed501b66d56886ef27d218082b677
pasindu P thennakoon <pasinduthennakoon512@gmail.com>
2025-07-02 12:16:41 +0530

    add empty star to rating.js

--- src/components/Rating/Rating.js
+++ src/components/Rating/Rating.js
@@ -1,5 +1,6 @@
 import React, { useMemo } from 'react'
 import SvgStarIcon from '../common/SvgStarIcon';
+import SvgEmptyStar from '../common/SvgEmptyStar';
 
 const Rating = ({rating}) => {
 
@@ -12,6 +13,12 @@ const Rating = ({rating}) => {
         {ratingNumber?.map((_,index)=>(
             <SvgStarIcon key={index}/>
         ))}
+        {
+            new Array(5-ratingNumber?.length).fill().map((_,index)=>(
+                <SvgEmptyStar key={'empty-'+index}/>
+            ))
+        }
+        <p className='px-2 text-gray-600'>{rating}</p>
     </div>
   )
 }
